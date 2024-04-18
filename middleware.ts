e057204bd85c3b045f46c6c0f6e2ed4aea0cfdc1
shaklan2001 <nishant.ns171@gmail.com>
2024-04-18 14:54:55 +0530

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,18 @@
-import { NextResponse } from "next/server";
-import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
- 
-export default authMiddleware({
-  afterAuth(auth, req, evt) {
-    // Handle users who aren't authenticated
-    if (!auth.userId && !auth.isPublicRoute) {
-      return redirectToSignIn({ returnBackUrl: req.url });
-    }
-    // Redirect signed in users to organization selection page if they are not active in an organization
-    if (
-      auth.userId &&
-      !auth.orgId &&
-      req.nextUrl.pathname !== "/"
-    ) {
-      const orgSelection = new URL("/sign-up", req.url);
-      return NextResponse.redirect(orgSelection);
-    }
-    // If the user is signed in and trying to access a protected route, allow them to access route
-    if (auth.userId && !auth.isPublicRoute) {
-      return NextResponse.next();
-    }
-    // Allow users visiting public routes to access them
-    return NextResponse.next();
-  },
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const protectedRoute = createRouteMatcher([
+  '/',
+  '/upcoming',
+  '/meeting(.*)',
+  '/previous',
+  '/recordings',
+  '/personal-room',
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (protectedRoute(req)) auth().protect();
 });
+
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
